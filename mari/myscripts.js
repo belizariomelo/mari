dae8282deb930288373275f244d3ce5abb033d6b
belizariomelo <124016228+belizariomelo@users.noreply.github.com>
2023-03-14 03:50:16 -0300

    Add files via upload

---
+++ mari/myscripts.js
@@ -0,0 +1,62 @@
+function abrirFormulario() {
+  document.getElementById("formulario").style.display = "block";
+}
+
+function fecharFormulario(event) {
+  event.preventDefault(); // Impede o comportamento padrão do botão
+  document.getElementById("formulario").style.display = "none";
+}
+// abrir/fechar o menu hamburguer ao clicar
+var menuToggle = document.querySelector(".menu-toggle");
+var nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  menuToggle.classList.toggle("open");
+  nav.classList.toggle("open");
+});
+new Glide(".glide", {
+  type: "carousel",
+  perView: 1,
+  focusAt: "center",
+  autoplay: 3500,
+  hoverpause: true,
+  breakpoints: {
+    640: {
+      perView: 1.25,
+    },
+  },
+  animationDuration: 500,
+}).mount();
+const form = document.getElementById("formulario");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const procedimento = document.getElementById("procedimento").value;
+
+  const data = new Date(document.getElementById("data").value);
+  const dataFormatada = data.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+
+  const horario = document.getElementById("horario").value;
+  const texto = `Olá, meu nome é ${nome}. Gostaria de agendar um(a) ${procedimento} para o dia ${dataFormatada} às ${horario}.`;
+
+  const api = `https://api.whatsapp.com/send?phone=5561986727887&text=${encodeURIComponent(
+    texto
+  )}`;
+  window.location.href = api;
+});
+const dataInput = document.getElementById("data");
+
+// Adiciona as barras ao inserir a data
+dataInput.addEventListener("input", function (event) {
+  let value = event.target.value;
+  value = value.replace(/\D/g, ""); // remove todos os caracteres não numéricos
+  value = value.replace(/(\d{2})(\d)/, "$1/$2"); // adiciona a primeira barra
+  value = value.replace(/(\d{2})(\d)/, "$1/$2"); // adiciona a segunda barra
+  event.target.value = value;
+});
